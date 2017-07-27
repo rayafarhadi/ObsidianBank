62a35bf68d6c588f5b745c0b09383756e6004dd2
farhadir <farhadir@UTORONTO.CA>
2017-07-27 02:56:06 +0000

    Added essential banking app project files

---
+++ app/src/main/java/com/bank/uitest/terminal/atm/ATMActivity.java
@@ -0,0 +1,18 @@
+package com.bank.uitest.terminal.atm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.bank.uitest.R;
+
+/**
+ * Created by rayafarhadi on 26/07/17.
+ */
+
+public class ATMActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atm);
+    }
+}
