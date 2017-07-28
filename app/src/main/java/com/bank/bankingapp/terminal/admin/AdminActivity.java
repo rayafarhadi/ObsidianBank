af57654a869c412be7dc4414c27643ac58052066
farhadir <farhadir@UTORONTO.CA>
2017-07-28 04:39:42 +0000

    Added admin terminal button functionality

--- app/src/main/java/com/bank/bankingapp/terminal/admin/AdminActivity.java
+++ app/src/main/java/com/bank/bankingapp/terminal/admin/AdminActivity.java
@@ -1,7 +1,10 @@
 package com.bank.bankingapp.terminal.admin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bank.bankingapp.R;
 
@@ -14,5 +17,65 @@ public class AdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        if (findViewById(R.id.admin_fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            AdminMessagesFragment messagesFragment = new AdminMessagesFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.admin_fragment_container, messagesFragment).commit();
+        }
     }
+
+    public void displayCreateUser(View view) {
+        AdminCreateUserFragment createUserFragment = new AdminCreateUserFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.admin_fragment_container, createUserFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    public void displayMessages(View view) {
+        AdminMessagesFragment messagesFragment = new AdminMessagesFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.admin_fragment_container, messagesFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    public void displayViewUser(View view) {
+        AdminViewUserFragment viewUserFragment = new AdminViewUserFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.admin_fragment_container, viewUserFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    public void displayTotalBalance(View view) {
+        AdminBalanceFragment balanceFragment = new AdminBalanceFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.admin_fragment_container, balanceFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    public void displayPromote(View view) {
+        AdminPromoteFragment promoteFragment = new AdminPromoteFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.admin_fragment_container, promoteFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
 }
