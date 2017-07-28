aec1bbe63e5eccf715939b4657c8d1f1b0dc9b4b
farhadir@UTORONTO.CA <farhadir@UTORONTO.CA@d6eb99c7-f7a0-4297-9e63-861829a56e4e>
2017-07-28 07:18:22 +0000

    Added atm terminal navigation

--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/terminal/atm/ATMActivity.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/terminal/atm/ATMActivity.java
@@ -1,9 +1,15 @@
 package com.bank.bankingapp.terminal.atm;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import com.bank.bankingapp.R;
+import com.bank.bankingapp.terminal.atm.fragments.ATMBalanceFragment;
+import com.bank.bankingapp.terminal.atm.fragments.ATMDepositFragment;
+import com.bank.bankingapp.terminal.atm.fragments.ATMListAccountsBalancesFragment;
+import com.bank.bankingapp.terminal.atm.fragments.ATMWithdrawFragment;
 
 /**
  * Created by rayafarhadi on 26/07/17.
@@ -14,5 +20,57 @@ public class ATMActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atm);
+
+
+        if (findViewById(R.id.atm_fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            ATMListAccountsBalancesFragment listAccountsBalancesFragment = new ATMListAccountsBalancesFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.atm_fragment_container, listAccountsBalancesFragment).commit();
+        }
     }
+
+    public void displayListAccountsBalances(View view) {
+        ATMListAccountsBalancesFragment listAccountsBalancesFragment = new ATMListAccountsBalancesFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.atm_fragment_container, listAccountsBalancesFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    public void displayATMWithdraw(View view) {
+        ATMWithdrawFragment withdrawFragment = new ATMWithdrawFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.atm_fragment_container, withdrawFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    public void displayATMDeposit(View view) {
+        ATMDepositFragment depositFragment = new ATMDepositFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.atm_fragment_container, depositFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+    public void displayAccountBalance(View view) {
+        ATMBalanceFragment balanceFragment = new ATMBalanceFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.atm_fragment_container, balanceFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
+    }
+
+
 }
