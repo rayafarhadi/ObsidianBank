fde984d55af81f537da796fa63c8eaa65a1e7b85
farhadir@UTORONTO.CA <farhadir@UTORONTO.CA@d6eb99c7-f7a0-4297-9e63-861829a56e4e>
2017-07-30 07:06:24 +0000

    Fixed terminal class

--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/terminals/Terminal.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/terminals/Terminal.java
@@ -27,4 +27,69 @@ public class Terminal {
                 currentUser.getRoleId() == designatedUserId);
         return this.authenticated;
     }
+
+    /**
+     * Returns the sum of all account balances a given user has with the bank.
+     *
+     * @param userId id of the user
+     * @return the sum of all balances
+     */
+    public BigDecimal addBalances(int userId) throws SQLException {
+        // Make sure terminal is authenticated
+        if (!authenticated) {
+            return null;
+        }
+
+        BigDecimal sum = new BigDecimal(0);
+        User customer = DatabaseSelectHelper.getUserDetails(userId);
+
+        // check that the customer is a customer, and makes sure it returns null if the account doesn't exist
+        if ((customer == null) || (customer.getRoleId() != Bank.rolesMap.get(Roles.CUSTOMER).getId())) {
+            return null;
+        }
+        for (Account account : ((Customer) customer).getAccounts()) {
+            sum = sum.add(account.getBalance());
+        }
+        return sum;
+    }
+
+    /**
+     * Returns the sum of all account balances.
+     *
+     * @return the sum of all balances
+     */
+    public BigDecimal addAllBalances() throws SQLException {
+        BigDecimal sum = new BigDecimal(0);
+        int accountId = 1;
+        // Make sure terminal is authenticated
+        if (!authenticated) {
+            return null;
+        }
+        Account account = DatabaseSelectHelper.getAccountDetails(accountId);
+        while (account != null) {
+            sum = sum.add(account.getBalance());
+            account = DatabaseSelectHelper.getAccountDetails(accountId);
+            accountId++;
+        }
+        return sum;
+    }
+
+    /**
+     * Gets an array list of current messages.
+     *
+     * @return list of messages.
+     * @throws SQLException
+     */
+    public ArrayList<Message> viewAllMessages() {
+        if (!authenticated) {
+            System.out.println("User not authenticated!");
+            return null;
+        }
+        try {
+            return DatabaseSelectHelper.getAllMessages(currentUser.getId());
+        } catch (SQLException e) {
+            System.out.println("Current user's user id does not exist in the database.");
+            return null;
+        }
+    }
 }
