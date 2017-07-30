c6d0920d3fcb35cc064f6c588d7128c6340bd9fd
farhadir@UTORONTO.CA <farhadir@UTORONTO.CA@d6eb99c7-f7a0-4297-9e63-861829a56e4e>
2017-07-30 06:44:11 +0000

    BankingApp: added contexts to a couple methods

--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/user/Customer.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/user/Customer.java
@@ -9,63 +9,62 @@ import com.bank.bankingapp.generics.Roles;
  */
 public class Customer extends User implements Serializable{
 
-  private static final long serialVersionUID = 7199924054172880898L;
-  private List<Account> accounts = new ArrayList<>();
+    private List<Account> accounts = new ArrayList<>();
 
 
-  /**
-   * Constructs a customer
-   *
-   * @param id customer's id
-   * @param name customer's name
-   * @param age customer's age
-   * @param address customer's address
-   */
-  public Customer(int id, String name, int age, String address) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.address = address;
-    this.roleId = Bank.rolesMap.get(Roles.CUSTOMER).getId();
-    this.authenticated = false;
-  }
+    /**
+     * Constructs a customer
+     *
+     * @param id      customer's id
+     * @param name    customer's name
+     * @param age     customer's age
+     * @param address customer's address
+     */
+    public Customer(int id, String name, int age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.roleId = Bank.rolesMap.get(Roles.CUSTOMER).getId();
+        this.authenticated = false;
+    }
 
-  /**
-   * Constructs a customer, and sets its authentication
-   *
-   * @param id user's id
-   * @param name user's name
-   * @param age user's age
-   * @param address user's address
-   */
-  public Customer(int id, String name, int age, String address, boolean authenticated) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.address = address;
-    this.roleId = Bank.rolesMap.get(Roles.CUSTOMER).getId();
-    this.authenticated = authenticated;
-  }
+    /**
+     * Constructs a customer, and sets its authentication
+     *
+     * @param id      user's id
+     * @param name    user's name
+     * @param age     user's age
+     * @param address user's address
+     */
+    public Customer(int id, String name, int age, String address, boolean authenticated) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.roleId = Bank.rolesMap.get(Roles.CUSTOMER).getId();
+        this.authenticated = authenticated;
+    }
 
-  /**
-   * Returns a list the customer's accounts
-   *
-   * @return a list of accounts
-   * @throws SQLException if connection fails
-   */
-  public List<Account> getAccounts() throws SQLException {
-    return accounts;
-  }
+    /**
+     * Returns a list the customer's accounts
+     *
+     * @return a list of accounts
+     * @throws SQLException if connection fails
+     */
+    public List<Account> getAccounts() {
+        return accounts;
+    }
 
-  /**
-   * Add's an account to this customer
-   *
-   * @param account account to add
-   * @throws SQLException if connection fails
-   */
-  public void addAccount(Account account) throws SQLException {
-    this.accounts.add(account);
-  }
+    /**
+     * Add's an account to this customer
+     *
+     * @param account account to add
+     * @throws SQLException if connection fails
+     */
+    public void addAccount(Account account) {
+        this.accounts.add(account);
+    }
 
 
 }
--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/user/UserFactory.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/user/UserFactory.java
@@ -1,5 +1,12 @@
 package com.bank.user;
 
+import android.content.Context;
+
+import com.bank.bankingapp.bank.Bank;
+import com.bank.bankingapp.database.DatabaseHelper;
+import com.bank.bankingapp.database.DatabaseSelectHelper;
+import com.bank.bankingapp.generics.Roles;
+
 import java.sql.SQLException;
 
 import com.bank.bank.Bank;
@@ -8,35 +15,35 @@ import com.bank.generics.Roles;
 
 public class UserFactory {
 
-  /**
-   * Builds and returns a user based on the given user role and user information
-   * 
-   * @param roleId The id of the users role
-   * @param userId The id of the user
-   * @param name The name of the user
-   * @param age The age of the user
-   * @param address The address of the user
-   * @return The user built based on given information
-   * @throws SQLException When the connection is lost with the database
-   */
-  public static User getUser(int roleId, int userId, String name, int age, String address)
-      throws SQLException {
+    /**
+     * Builds and returns a user based on the given user role and user information
+     *
+     * @param roleId  The id of the users role
+     * @param userId  The id of the user
+     * @param name    The name of the user
+     * @param age     The age of the user
+     * @param address The address of the user
+     * @return The user built based on given information
+     */
+    public static User getUser(int roleId, int userId, String name, int age, String address, Context context) {
 
-    // If the role id matches to admin then build an admin account
-    if (roleId == Bank.rolesMap.get(Roles.ADMIN).getId()) {
-      return new Admin(userId, name, age, address);
+        DatabaseHelper db = new DatabaseHelper(context);
+
+        // If the role id matches to admin then build an admin account
+        if (roleId == Bank.rolesMap.get(Roles.ADMIN).getId()) {
+            return new Admin(userId, name, age, address);
 
       // If the role id matches to teller then build an teller account
     } else if (roleId == Bank.rolesMap.get(Roles.TELLER).getId()) {
       return new Teller(userId, name, age, address);
 
-      // If the role id matches to customer then build an customer account
-    } else if (roleId == Bank.rolesMap.get(Roles.CUSTOMER).getId()) {
-      Customer ret = new Customer(userId, name, age, address);
-      for (int accId : DatabaseSelectHelper.getAccountIds(userId)) {
-        ret.addAccount(DatabaseSelectHelper.getAccountDetails(accId));
-      }
-      return ret;
+            // If the role id matches to customer then build an customer account
+        } else if (roleId == Bank.rolesMap.get(Roles.CUSTOMER).getId()) {
+            Customer ret = new Customer(userId, name, age, address);
+            for (int accId : db.getAccountIds(userId)) {
+                ret.addAccount(db.getAccountDetails(accId));
+            }
+            return ret;
 
       // Otherwise return an exception
     } else {
