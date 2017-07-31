b5991f5d490ab725a5342ca8aacf6e3323f9d874
mawesley <mawesley@UTORONTO.CA>
2017-07-31 14:04:44 +0000

    App: added messages for teller terminal

--- app/src/main/java/com/bank/bankingapp/activities/terminal/teller/fragments/TellerMessagesFragment.java
+++ app/src/main/java/com/bank/bankingapp/activities/terminal/teller/fragments/TellerMessagesFragment.java
@@ -5,8 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.bank.bankingapp.R;
+import com.bank.bankingapp.activities.terminal.admin.fragments.AdminMessagesAdapter;
+import com.bank.bankingapp.activities.terminal.teller.TellerActivity;
+import com.bank.bankingapp.database.DatabaseHelper;
+import com.bank.bankingapp.messages.Message;
+import com.bank.bankingapp.terminals.TellerTerminal;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by rayafarhadi on 31/07/17.
@@ -16,5 +25,28 @@ public class TellerMessagesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.view_messages, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        TellerActivity prevActivity = (TellerActivity) this.getActivity();
+        TellerTerminal tt = prevActivity.getTt();
+        DatabaseHelper db = new DatabaseHelper(getContext());
+        List<Message> messages = new ArrayList<>();
+        // Get unread then read messages
+        for (Message message : db.getAllMessages(tt.getCurrentUser().getId())){
+            if (!message.isViewed()){
+                messages.add(message);
+            }
+        }
+        for (Message message : db.getAllMessages(tt.getCurrentUser().getId())){
+            if (message.isViewed()){
+                messages.add(message);
+            }
+        }
+
+        AdminMessagesAdapter adapter = new AdminMessagesAdapter(this.getContext(), messages);
+        ListView usersInfo = getView().findViewById(R.id.messages);
+        usersInfo.setAdapter(adapter);
     }
 }
