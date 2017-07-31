94ce3a95e7f9872bd74b5a84d2066e268582d71f
mawesley@UTORONTO.CA <mawesley@UTORONTO.CA@d6eb99c7-f7a0-4297-9e63-861829a56e4e>
2017-07-31 02:24:40 +0000

    App: implemented list account info in ATM

--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/atm/fragments/ATMListAccountsBalancesFragment.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/atm/fragments/ATMListAccountsBalancesFragment.java
@@ -1,12 +1,22 @@
 package com.bank.bankingapp.activities.terminal.atm.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.bank.bankingapp.R;
+import com.bank.bankingapp.account.Account;
+import com.bank.bankingapp.activities.terminal.atm.ATMActivity;
+import com.bank.bankingapp.terminals.ATM;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.Attributes;
 
 /**
  * Created by rayafarhadi on 26/07/17.
@@ -15,9 +25,22 @@ import com.bank.bankingapp.R;
 public class ATMListAccountsBalancesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        ATMActivity prevActivity = (ATMActivity) this.getActivity();
+        ATM atm = prevActivity.getAtm();
+        List<Account> accounts = new ArrayList<>();
+        try {
+            accounts = atm.listAccounts();
+        } catch (SQLException e) {
+            System.out.println("Error when listing accs");
+            e.printStackTrace();
+        }
+        AccountInfoAdapter adapter = new AccountInfoAdapter(getContext(), accounts);
+        ListView accountsInfo = (ListView) getView().findViewById(R.id.accounts_and_balances);
+        accountsInfo.setAdapter(adapter);
 
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.view_atm_list_account_balance, container, false);
     }
+
 
 }
