e8f3c0b7fa1ae491b636acb891c69e0a487f1bcb
farhadir@UTORONTO.CA <farhadir@UTORONTO.CA@d6eb99c7-f7a0-4297-9e63-861829a56e4e>
2017-07-31 07:13:01 +0000

    Fixed total balance issue

--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/admin/fragments/AdminAllBalanceFragment.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/admin/fragments/AdminAllBalanceFragment.java
@@ -20,12 +20,8 @@ public class AdminAllBalanceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         RelativeLayout rl = (RelativeLayout) inflater.inflate(R.layout.view_admin_all_balance, container, false);
-        balance = rl.findViewById(R.id.admin_balance);
-        updateBalance();
-        return rl;
-    }
+        balance = (TextView) rl.findViewById(R.id.admin_total_balance);
 
-    public void updateBalance() {
         AdminTerminal at = new AdminTerminal(this.getContext());
         BigDecimal total_balance = at.addAllBalances();
         if (total_balance == null) {
@@ -33,5 +29,7 @@ public class AdminAllBalanceFragment extends Fragment {
         }
 
         balance.setText(total_balance.toString());
+
+        return rl;
     }
 }
--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/admin/fragments/AdminPromoteFragment.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/admin/fragments/AdminPromoteFragment.java
@@ -5,23 +5,27 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.bank.bankingapp.R;
 import com.bank.bankingapp.terminals.AdminTerminal;
 
-/**
- * Created by rayafarhadi on 28/07/17.
- */
-
 public class AdminPromoteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LinearLayout ll = (LinearLayout) inflater.inflate(R.layout.view_admin_promote_teller, container, false);
+
+        Spinner spinner = (Spinner) getView().findViewById(R.id.admin_promote_teller_spinner);
+
+        //SpinnerAdapter adapter = SpinnerAdapter.createFromResource(this);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.view_admin_promote_teller, container, false);
+        return ll;
     }
 
-    public void adminPromote(){
+    public void adminPromote() {
         AdminTerminal at = new AdminTerminal(this.getContext());
 
     }
