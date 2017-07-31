94404f953a1c5591f2dafba3b799b62991a78286
sokolonj@UTORONTO.CA <sokolonj@UTORONTO.CA@d6eb99c7-f7a0-4297-9e63-861829a56e4e>
2017-07-31 04:01:19 +0000

    BankingApp: Fixed some input fields in Adminterminal when creating user.

--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/admin/fragments/AdminPromoteFragment.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/admin/fragments/AdminPromoteFragment.java
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.bank.bankingapp.R;
+import com.bank.bankingapp.terminals.AdminTerminal;
 
 /**
  * Created by rayafarhadi on 28/07/17.
@@ -17,5 +19,10 @@ public class AdminPromoteFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.view_admin_promote_teller, container, false);
+    }
+
+    public void adminPromote(){
+        AdminTerminal at = new AdminTerminal(this.getContext());
+
     }
 }
