e4a08bc2426889453cbd8397aa1d42d63e6fa3dc
sokolonj <sokolonj@UTORONTO.CA>
2017-07-31 16:15:40 +0000

    BankingApp: Added Dialog Window when depositing or withdrawing from ATM

--- app/src/main/java/com/bank/bankingapp/activities/terminal/atm/fragments/ATMWithdrawFragment.java
+++ app/src/main/java/com/bank/bankingapp/activities/terminal/atm/fragments/ATMWithdrawFragment.java
@@ -1,8 +1,10 @@
 package com.bank.bankingapp.activities.terminal.atm.fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -74,19 +76,25 @@ public class ATMWithdrawFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 EditText message_field = getView().findViewById(R.id.withdraw_amount);
+                final AlertDialog.Builder idNotification = new AlertDialog.Builder(getContext());
+                idNotification.setTitle(R.string.dialog_create_title);
                 try {
                     atm.makeWithdrawal(new BigDecimal(message_field.getText().toString()), targetAccount.getId());
+                    idNotification.setMessage(message_field.getText().toString() + "$ has been successfully withdrawn from account.");
                 } catch (IllegalAmountException e){
-                    Toast toast = Toast.makeText(getContext(), "Illegal Amount", Toast.LENGTH_SHORT);
-                    toast.show();
+                    idNotification.setMessage("Cannot withdraw a negative amount.");
                 } catch (ConnectionFailedException e){
-                    Toast toast = Toast.makeText(getContext(), "Connection Failed", Toast.LENGTH_SHORT);
-                    toast.show();
+                    idNotification.setMessage("Error: Connection to database failed.");
                 } catch (InsuffiecintFundsException e){
-                    Toast toast = Toast.makeText(getContext(), "Insufficient Funds", Toast.LENGTH_SHORT);
-                    toast.show();
+                    idNotification.setMessage("Cannot withdraw more then current total balance.");
                 }
+                idNotification.setPositiveButton(R.string.dialog_ok, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
                 message_field.setText("");
+                idNotification.create();
+                idNotification.show();
             }
         });
 
