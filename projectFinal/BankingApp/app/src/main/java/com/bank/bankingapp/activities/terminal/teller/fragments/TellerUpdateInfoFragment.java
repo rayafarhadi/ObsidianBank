b50debf6fe173a3d7e2bc2f1b3092b9ed62ef7aa
sokolonj@UTORONTO.CA <sokolonj@UTORONTO.CA@d6eb99c7-f7a0-4297-9e63-861829a56e4e>
2017-08-01 02:08:50 +0000

    BankingApp: Added javadoc for TellerFragments

--- projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/teller/fragments/TellerUpdateInfoFragment.java
+++ projectFinal/BankingApp/app/src/main/java/com/bank/bankingapp/activities/terminal/teller/fragments/TellerUpdateInfoFragment.java
@@ -12,10 +12,12 @@ import com.bank.bankingapp.R;
  * Created by rayafarhadi on 31/07/17.
  */
 
+/**
+ * Inflates the current layout into the fragment.
+ */
 public class TellerUpdateInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.view_teller_update_info, container, false);
     }
-
 }
